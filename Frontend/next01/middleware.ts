1213ee7ee1082e934f5258bbc71b42f6dde200a6
Phan Nam <namph03f8@fullstack.edu.vn>
2026-06-13 22:31:13 +0700

    ...

--- Frontend/next01/middleware.ts
+++ Frontend/next01/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { decodeJwt } from 'jose';
+import { decodeJwt } from 'jose/jwt/decode'; 
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
